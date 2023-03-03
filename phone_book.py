d48e692afb0b1344a27504452c4f5a107805c5d1
Mansi Nerkar <mansinerkar11@gmail.com>
2023-03-03 18:32:01 +0530

    basic codes

--- phone_book.py
+++ phone_book.py
@@ -1,8 +1,8 @@
 """Practical No 4        Roll No.: 77         class:SE-B
-a) Write a Python program to store names and mobile numbers of your friends in sorted order on names. 
+a)Write a Python program to store names and mobile numbers of your friends in sorted order on names. 
 Search your friend from list using binary search (recursive and non- recursive). 
 Insert friend if not present in phonebook
-b) Write a Python program to store names and mobile numbers of your friends in sorted order on names. 
+b)Write a Python program to store names and mobile numbers of your friends in sorted order on names. 
 Search your friend from list using Fibonacci/Linear search. Insert friend if not present in phonebook.
 """
 #Binary Search 
