186451c4d4761168e013a1ab2f7992b11e9e829d
Mansi Nerkar <mansinerkar11@gmail.com>
2023-03-03 22:39:45 +0530

    basic codes

--- area_ractangle.py
+++ area_ractangle.py
@@ -1,4 +1,4 @@
 no1=int(input("Enter the Length of ractangle: "))
 no2=int(input("Enter the Breadth of ractangle: "))
 no3=no1*no2
-print("Area of ractangle is: ",no3)
+print("Area of ractangle is: ",no3) #display area
